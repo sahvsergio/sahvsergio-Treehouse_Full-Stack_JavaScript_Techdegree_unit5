825c0d7200238646c2b683bed972bdbe1ab86ad6
sahvsergio <sergioandresherrera@gmail.com>
2025-02-22 14:21:38 -0500

    fetch the 12 results

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,35 @@
-let peopleUrl = "https://randomuser.me/api/?results=12";;
+let peopleUrl = "https://randomuser.me/api/?results=12";
+
+fetch(peopleUrl)
+.then(response=>response.json())
+.then(res=>console.log(res.results))
 
 
+/*
+ display 12 users, along with some basic information for each:
+
+    Image
+    First and Last Name
+    Email
+    City or location
+    */
+/*
 function getUsers(url){
-
-    const xhr= new XMLHttpRequest();
+    return new Promise((resolve, reject)=>{
+         const xhr= new XMLHttpRequest();
     xhr.open('GET', url);
     xhr.onload= ()=>{
 
         if(xhr.status===200){
             let data=JSON.parse(xhr.responseText);
-            console.log(data);
+            resolve(data);
+        }
+        else{
+            reject(Error(xhr.statusText));
         }
     };
+    xhr.onerror=()=>reject(Error('A network occurred'));
     xhr.send();
-    
-    
+});
 }
+getUsers(peopleUrl);*/
